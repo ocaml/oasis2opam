84eb05074fdc1ecdd705829660bdad3af951a841
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2014-08-24 23:45:05 +0200

    Camlp4 was split from the OCaml distribution
    A dummy OPAM package exists for OCaml ≤ 4.01.0

--- src/BuildDepends.ml
+++ src/BuildDepends.ml
@@ -33,7 +33,7 @@ let opam_base_packages = [ "bigarray", "base-bigarray";
 
 (* Findlib libraries coming with OCaml — no OPAM package. *)
 let findlib_with_ocaml =
-  let pkg = [ "camlp4"; "dynlink"; "graphics"; "labltk"; "num";
+  let pkg = [ "dynlink"; "graphics"; "labltk"; "num";
               "ocamlbuild"; "stdlib"; "str"; "compiler-libs" ] in
   List.fold_left (fun s e -> S.add e s) S.empty pkg
 
