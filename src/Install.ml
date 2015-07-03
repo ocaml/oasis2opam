dad766ac28f872fee6af30a38ed6568f3cb07a11
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2015-07-04 00:08:16 +0200

    Add datafiles to the <package>.install file
    Only datafiles of installed sections are considered (of course).

--- src/Install.ml
+++ src/Install.ml
@@ -53,7 +53,6 @@ let with_install t ~local ~f =
   close fh
 
 let binaries t ~flags =
-  let pkg = Tarball.oasis t in
   let gather_exec bins = function
     | Executable(cs, bs, es) ->
        if eval_conditional flags bs.bs_install then (
@@ -66,21 +65,101 @@ let binaries t ~flags =
        )
        else bins
     | _ -> bins (* skip other sections *) in
-  List.fold_left gather_exec [] pkg.sections
+  List.fold_left gather_exec [] (Tarball.oasis t).sections
 
-let write_bin t fh bins =
-  output_string fh "bin: [\n";
-  let output_bin (exec, name) =
-    let exec = Utils.escaped exec in
-    let name = Utils.escaped name in
-    fprintf fh "  \"?%s.byte\" {\"%s\"}\n" exec name;
-    fprintf fh "  \"?%s.native\" {\"%s\"}\n" exec name;
-  in
-  List.iter output_bin bins;
-  output_string fh "]\n"
+(* (Subset of) Possible locations for OPAM. *)
+type dest_dir = Lib | Libexec | Bin | Sbin | Share | Etc | Doc
+
+let all_dest_dirs = [Lib; Libexec; Bin; Sbin; Share; Etc; Doc]
+
+let string_of_dest_dir = function
+  | Lib -> "lib"
+  | Libexec -> "libexec"
+  | Bin -> "bin"
+  | Sbin -> "sbin"
+  | Share -> "share"
+  | Etc -> "etc"
+  | Doc -> "doc"
+
+let default_dest =
+  (Share, "") (* the second component is the relative path *)
+
+let split_slash d =
+  try let i = String.index d '/' in
+      (String.sub d 0 i, String.sub d (i + 1) (String.length d - i - 1))
+  with Not_found -> (d, "")
+
+let classify_dest d =
+  let dest, dir = split_slash d in
+  if dest = "$libdir" then (Lib, dir)
+  else if dest = "$libexecdir" then (Libexec, dir)
+  else if dest = "$bindir" then (Bin, dir)
+  else if dest = "$sbindir" then (Sbin, dir)
+  else if dest = "$datadir" then (Share, dir)
+  else if dest = "$sysconfdir" then (Etc, snd(split_slash dir))
+                                      (* $sysconfdir/pkg/dir *)
+  else if dest = "$docdir" then (Doc, dir)
+  else (
+    warn(sprintf "Target %S not recognized, installing in share/" dest);
+    default_dest
+  )
+
+let classify_dest_opt = function
+  | Some dest -> classify_dest dest
+  | None -> default_dest
+
+let output_classified fh (local, (_, dir)) =
+  if dir = "" then fprintf fh "  %S\n" local
+  else let dest = Filename.concat dir (Filename.basename local) in
+       fprintf fh "  %S {%S}\n" local dest
+
+(* Gather all DataFiles. *)
+let datafiles t ~flags =
+  let pkg = Tarball.oasis t in
+  let gather_data_files datas = function
+    | Library(_, bs, _)
+    | Object(_, bs, _)
+    | Executable(_, bs, _) ->
+       if eval_conditional flags bs.bs_install then (
+         let d = bs.bs_data_files in
+         let d = List.map (fun (f, l) -> (Filename.concat bs.bs_path f,
+                                        classify_dest_opt l)) d in
+         d @ datas
+       )
+       else datas
+    | _ -> datas (* skip other sections *) in
+  List.fold_left gather_data_files [] pkg.sections
+
+
+let write_bin fh bins data_bin =
+  if bins <> [] || data_bin <> [] then (
+    output_string fh "bin: [\n";
+    let output_bin (exec, name) =
+      let exec = Utils.escaped exec in
+      let name = Utils.escaped name in
+      fprintf fh "  \"?%s.byte\" {\"%s\"}\n" exec name;
+      fprintf fh "  \"?%s.native\" {\"%s\"}\n" exec name;
+    in
+    List.iter output_bin bins;
+    List.iter (output_classified fh) data_bin;
+    output_string fh "]\n"
+  )
+
+let write_datas fh datas =
+  let write_dest dest_dir =
+    let datas = List.filter (fun (_, (d, _)) -> d = dest_dir) datas in
+    if datas <> [] then (
+      fprintf fh "%s: [\n" (string_of_dest_dir dest_dir);
+      List.iter (output_classified fh) datas;
+      output_string fh "]\n"
+    ) in
+  List.iter write_dest all_dest_dirs
 
 let opam t ~local flags =
   let bins = binaries t ~flags in
-  if bins <> [] then (
-    with_install t ~local ~f:(fun fh -> write_bin t fh bins)
+  let datas = datafiles t ~flags in
+  let data_bin, datas = List.partition (fun (_, (d, _)) -> d = Bin) datas in
+  if bins <> [] || datas <> [] then (
+    with_install t ~local ~f:(fun fh -> write_bin fh bins data_bin;
+                                      write_datas fh datas)
   )
