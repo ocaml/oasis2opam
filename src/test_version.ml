6123db4ebda0a2b7a3e0db21d818618a9b3fcc44
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2017-01-08 21:06:19 +0100

    Port the test to QCheck ≥ 0.5

--- src/test_version.ml
+++ src/test_version.ml
@@ -1,58 +1,52 @@
-open OUnit2
-open QCheck
 open OASISVersion
 
 let version_of_int i = OASISVersion.version_of_string (string_of_int i)
-let arbitrary_version = Arbitrary.(small_int >|= version_of_int)
+let arbitrary_version = QCheck.Gen.(small_int >|= version_of_int)
 
 let arbitrary_version_comparator =
-  Arbitrary.(retry (
-    small_int >>= fix_fuel [
-      `Base (arbitrary_version >>= fun v ->
-             among [VGreater v; VGreaterEqual v; VEqual v;
-                    VLesser v; VLesserEqual v]);
-      `Rec (fun self ->
-          self 2 >>= function
-          | [x;y] -> among [VOr (x,y); VAnd (x,y)]
-          | _ -> assert false
-        )]))
+  let open QCheck.Gen in
+  let leaf =
+    arbitrary_version >>= fun v ->
+    oneofl [VGreater v; VGreaterEqual v; VEqual v;
+            VLesser v; VLesserEqual v] in
+  let vor x y = VOr(x,y) in
+  let vand x y = VAnd(x,y) in
+  fix (fun self n ->
+      if n = 0 then leaf
+      else frequency [1, leaf;
+                      2, (let x = self (n/2) and y = self(n/2) in
+                          oneof [map2 vor x y; map2 vand x y])]
+    )
+  |> sized
+  |> QCheck.make ~print:Version.string_of_comparator
 
 let rec comparator_size = function
   | VGreater _ | VGreaterEqual _ | VEqual _ | VLesser _ | VLesserEqual _ -> 1
   | VOr (a, b) | VAnd (a, b) -> comparator_size a + comparator_size b
 
-let equivalent ~pp ~name ~size gen f is_equivalent =
-  let pp (v1, v2) =
-    Printf.sprintf "\ninput:(%s)\noutput:(%s)" (pp v1) (pp v2) in
-  let size (v, _) = size v in
-  mk_test ~n:1000 ~pp ~name ~size Arbitrary.(gen >|= fun v -> v, f v)
-          is_equivalent
+let equivalent ~name f is_equivalent =
+  QCheck.Test.make ~count:1000 ~name
+    (QCheck.map (fun v -> (v, f v)) arbitrary_version_comparator)
+    is_equivalent
+  |> QCheck.Test.check_exn
 
-let is_equivalent (a, b) =
-  (a = b)
-  || match check arbitrary_version
-                (fun v -> OASISVersion.comparator_apply v a
-                        = OASISVersion.comparator_apply v b) with
-    | Ok _ -> true
-    | Failed _ | Error _ -> false
-
-let is_inverse (a, b) =
-  match check arbitrary_version
-              (fun v -> OASISVersion.comparator_apply v a
-                      = not (OASISVersion.comparator_apply v b)) with
-  | Ok _ -> true
+let test_comparators a b test =
+  let res =
+    QCheck.Test.make_cell (QCheck.make arbitrary_version)
+      (fun v -> test (OASISVersion.comparator_apply v a)
+                     (OASISVersion.comparator_apply v b))
+    |> QCheck.Test.check_cell in
+  let open QCheck.TestResult in
+  match res.state with
+  | Success -> true
   | Failed _ | Error _ -> false
 
-let suite =
-  QCheck_ounit.to_ounit_suite [
-      equivalent ~pp:Version.string_of_comparator
-                 ~name:"comparator_reduce"
-                 ~size:comparator_size arbitrary_version_comparator
-                 Version.comparator_reduce is_equivalent;
-      equivalent ~pp:Version.string_of_comparator
-                 ~name:"complement_reduce"
-                 ~size:comparator_size arbitrary_version_comparator
-                 Version.complement_reduce is_inverse;
-    ]
+let is_equivalent (a, b) = (a = b) || test_comparators a b ( = )
 
-let () = OUnit2.run_test_tt_main ("version" >::: suite)
+let is_inverse (a, b) = test_comparators a b (fun a b -> a = not b)
+
+let () =
+  equivalent ~name:"comparator_reduce"
+    Version.comparator_reduce is_equivalent;
+  equivalent ~name:"complement_reduce"
+    Version.complement_reduce is_inverse
